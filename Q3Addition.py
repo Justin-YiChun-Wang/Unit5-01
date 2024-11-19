0790b72c2a6160cb1709134f4b430346dd011bdd
Justin-YiChun-Wang <justin.wang@stu.ocsb.ca>
2024-11-19 10:40:34 -0500

    Create Q3Addition.py

---
+++ Q3Addition.py
@@ -0,0 +1,9 @@
+for x in range(1,100):
+    for y in range(1,100):
+        print(x, '+', y)
+        a = float(input())
+        if a == (x + y):
+            print('your answer is correct')
+        else:
+            print('Your answer is incorrect')
+END
