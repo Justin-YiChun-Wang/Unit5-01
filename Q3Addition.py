25f259ab9b28e96623e1bc7064dda913710d42a8
Justin-YiChun-Wang <justin.wang@stu.ocsb.ca>
2024-11-27 09:38:29 -0500

    Update Q3Addition.py

--- Q3Addition.py
+++ Q3Addition.py
@@ -1,9 +1,13 @@
-for x in range(1,100):
-    for y in range(1,100):
-        print(x, '+', y)
-        a = float(input())
-        if a == (x + y):
-            print('your answer is correct')
-        else:
-            print('Your answer is incorrect')
-END
+import random
+def hello_there():
+    min_value = 1
+    max_value = 100
+    num1 = random.randint(min_value, max_value)
+    num2 = random.randint(min_value, max_value)
+    print(num1, '+', num2)
+    a = int(input())
+    if a == num1 + num2:
+        print('Good job')
+    else:
+        print('So uncivilized')
+hello_there()
